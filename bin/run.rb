a3ad08215ef03f0296f0f1146e3a53d4fcac9dbf
finale100 <finale150@gmail.com>
2018-07-10 16:18:26 -0400

    improved run.rb methods

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,25 @@
 require_relative '../config/environment'
+require 'pry'
+
+def prompt
+  puts "What would you like to do?"
+  puts "1. View all Recipes"
+  puts "2. Log into Account"
+end
+
+def option_1
+  input = gets.chomp
+puts "Select a recipe!"
+  if input == "1"
+    Recipe.all.each_with_index {|x, index| puts "#{index}. #{x.name}" }
+  end
+end
+
+puts "Welcome to Tastybook!"
+prompt
+option_1
 
 
-
-puts "HELLO WORLD"
+# if input == "1"
+#   Recipe.all.each_with_index {|x, index| puts "#{index}. #{x.name}" }
+# end
